56ea4096101e5787fa6e9773519af30a9713d930
Ethal Askander <Ethal.Askander@outlook.com>
2021-02-23 18:37:02 +1100

    :wrench: Sorter now works and outputs as intended

--- sorter.py
+++ sorter.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import re
 
 def sort_references(references):
@@ -18,11 +19,35 @@ try:
     if (len(sys.argv) < 2):
         raise Exception('Please provide a file name')
     file_name = sys.argv[1]
+    if (not os.path.isfile(file_name)):
+        raise Exception('{} is not a file in this directory'.format(file_name))
     file_link = open(file_name)
 
     # Read file contents
     content = file_link.read().split('\n')
-    print(sort_references(content))
+
+    # Close link
+    file_link.close()
+
+    # Sort the content
+    content = sort_references(content)
+
+    # Open link to new 'sorted' file to write to
+    split_path = os.path.splitext(file_name)
+    base_name = split_path[0]
+    extension = split_path[1]
+    writer = open('{} (sorted){}'.format(base_name, extension), 'w')
+
+    # Output sorted content to a file
+    for ref in content:
+        # Check if empty string
+        if (len(ref) == 0):
+            continue
+
+        writer.write(ref + ('\n\n' if ref != content[len(content) - 1] else ''))
+
+    # Close link
+    writer.close()
 except Exception as err:
     print(err)
 
