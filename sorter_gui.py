7322eac14cedbee75d65adc6b8b1b8a97577709b
Ethal <u6669384@anu.edu.au>
2021-03-02 12:51:17 +1100

    feat: Rreferences are now be sorted and provided to the user by clicking 'sort' button

--- sorter_gui.py
+++ sorter_gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
+import re
 
 def sort_references(references):
     '''
@@ -12,7 +13,15 @@ def sort_references(references):
     '''
     # Ignore all non-latin characters and sort
     references = sorted(references, key=lambda x: re.sub('[^A-Za-z]+', '', x).lower())
-    return references
+    
+    # filter references
+    filtered_references = ""
+    for ref in references:
+        # Check if empty string or just new line
+        if (len(ref) == 0 or ref == "\n"):
+            continue
+        filtered_references += ref + ('\n\n' if ref != references[len(references) - 1] else '')
+    return filtered_references
 
 root = tk.Tk()
 root.maxsize(700, 700)
@@ -26,10 +35,22 @@ canvas.pack(pady=10)
 # frame = tk.Frame(root, bg="white")
 # frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-text_box = tk.Text(canvas, height=40, fg="black", bg="white")
+text_box = tk.Text(canvas, height=40, fg="white", bg="#263D42")
 text_box.pack()
 
-sort_button = tk.Button(root, text="Sort", padx=100, pady=5, fg="white", bg="#263D42")
+def sort_clicked():
+    '''
+    Gets current text, sorts it and replaced the current text with the sorted text.
+    '''
+    # Get the references
+    references = text_box.get("1.0", tk.END).split('\n')
+
+    # Sort the references
+    references = sort_references(references)
+
+    text_box.replace("1.0", tk.END, references)
+
+sort_button = tk.Button(root, text="Sort", padx=100, pady=5, fg="white", bg="black", command=sort_clicked)
 sort_button.pack()
 
 # Loop to run continously
