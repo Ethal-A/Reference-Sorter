6a94c798989b36b736842aa7ae1454030c1d7cec
Ethal Askander <Ethal.Askander@outlook.com>
2021-03-04 15:45:54 +1100

    refactor: defined background_colour parameter

--- sorter_gui.py
+++ sorter_gui.py
@@ -4,6 +4,7 @@ import os
 import re
 
 # Parameters
+background_colour = '#263D42'
 app_width = 700
 app_height = 700
 
@@ -33,14 +34,11 @@ root.maxsize(app_width, app_height)
 root.minsize(app_width, app_height)
 
 # Defining GUI layout
-canvas = tk.Canvas(root, height=app_height, width=app_width, bg="#263D42")
+canvas = tk.Canvas(root, height=app_height, width=app_width, bg=background_colour)
 canvas.pack(pady=10)
 
-# Frame (inside) layout
-# frame = tk.Frame(root, bg="white")
-# frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-text_box = tk.Text(canvas, height=40, fg="white", bg="#263D42")
+# Input
+text_box = tk.Text(canvas, height=40, fg="white", bg=background_colour)
 text_box.pack()
 
 def help_clicked():
@@ -58,10 +56,10 @@ def sort_clicked():
 
     text_box.replace("1.0", tk.END, references)
 
-sort_button = tk.Button(root, text="Sort", padx=50, pady=5, fg="white", bg="#263D42", command=sort_clicked)
+sort_button = tk.Button(root, text="Sort", padx=50, pady=5, fg="white", bg=background_colour, command=sort_clicked)
 sort_button.place(x=(app_width / 2 - 50), y=660)
 
-help_button = tk.Button(root, text="Help", padx=20, pady=5, fg="white", bg="#263D42", command=help_clicked)
+help_button = tk.Button(root, text="Help", padx=20, pady=5, fg="white", bg=background_colour, command=help_clicked)
 help_button.place(x=27,y=660)
 
 # Loop to run continously
